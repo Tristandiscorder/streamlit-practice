b46d08a1b820a586367eff687cdaf6d5c1f3cace
Tristandiscorder <ohsama9114@gmail.com>
2023-10-16 10:53:52 +0900

    initial config

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+
+st.set_page_config(
+    page_icon=":book:"
+    page_title='practice'
+    layout='wide',
+)
